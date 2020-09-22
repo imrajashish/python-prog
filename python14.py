fda71484c2ffba5f5ed9fe935fac754357e2e2cb
imrajashish <imrajashish07@gmail.com>
2020-09-22 20:10:36 +0530

    math

---
+++ python14.py
@@ -0,0 +1,11 @@
+#Write a Python program to convert degree to radian. 
+pi=22/7
+degree = float(input("Input degrees: "))
+radian = degree*(pi/180)
+print(radian)
+
+#Write a Python program to convert radian to degree.
+pi=22/7
+radian = float(input("Input radians: "))
+degree = radian*(180/pi)
+print(degree)
