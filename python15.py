8d571056ad833dacb19073de4b6cf204d543408e
imrajashish <imrajashish07@gmail.com>
2020-09-23 18:43:50 +0530

    maths

---
+++ python15.py
@@ -0,0 +1,14 @@
+#Write a Python program to calculate the area of a trapezoid.
+base_1 = 5
+base_2 = 6
+height = float(input("Height of trapezoid: "))
+base_1 = float(input('Base one value: '))
+base_2 = float(input('Base two value: '))
+area = ((base_1 + base_2) / 2) * height
+print("Area is:", area)
+
+# Write a Python program to calculate the area of a parallelogram.
+base = float(input('Length of base: '))
+height = float(input('Measurement of height: '))
+area = base * height
+print("Area is: ", area)
