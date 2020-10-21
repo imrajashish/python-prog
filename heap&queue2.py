fd451b219d39d339e6e49a1389696ad927e178f1
imrajashish <imrajashish07@gmail.com>
2020-10-21 19:45:11 +0530

    heap queue ,list

--- heap&queue2.py
+++ heap&queue2.py
@@ -74,6 +74,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :type k: int
         :rtype: int
+        """
         m, n = len(matrix), len(matrix[0])
         temp = [[False] * n for _ in range(m)]
         q = [(matrix[0][0], 0, 0)]
@@ -106,6 +107,7 @@ k = 3
 s = Solution()
 result = s.find_Kth_Smallest(matrix, k)
 print("\nThird smallest element:",result)
+
 
 #Write a Python program to find the nth super ugly number from a given prime list of size k using Heap queue algorithm
 import heapq
