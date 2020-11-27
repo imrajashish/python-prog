306582613e5ed2c6ab0731fcc561595949f143f2
imrajashish <imrajashish07@gmail.com>
2020-11-27 21:11:38 +0530

    class

---
+++ class23.py
@@ -0,0 +1,61 @@
+#Write a Python program which add integer numbers from the data stream to a heapq and compute the median of all elements. Use Heap queue algorithm.
+import heapq
+class Median_Finder:
+    
+    def __init__(self):
+        #initialize data structure
+        self.max_heap = []
+        self.min_heap = []
+        
+
+    def add_Number(self, num):
+        #type num: int, rtype: void
+        if not self.max_heap and not self.min_heap:
+            heapq.heappush(self.min_heap, num)
+            return 
+        if not self.max_heap:
+            if num > self.min_heap[0]:
+                heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+                heapq.heappush(self.min_heap, num)
+            else:
+                heapq.heappush(self.max_heap, -num)
+            return
+        if len(self.max_heap) == len(self.min_heap):
+            if num < -self.max_heap[0]:
+                heapq.heappush(self.max_heap, -num)
+            else:
+                heapq.heappush(self.min_heap, num)
+        elif len(self.max_heap) > len(self.min_heap):
+            if num < -self.max_heap[0]:
+                heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+                heapq.heappush(self.max_heap, -num)
+            else:
+                heapq.heappush(self.min_heap, num)
+        else:
+            if num > self.min_heap[0]:
+                heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+                heapq.heappush(self.min_heap, num)
+            else:
+                heapq.heappush(self.max_heap, -num)
+        
+
+    def find_Median(self):
+        #rtype: float
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        elif len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return self.min_heap[0] 
+S = Median_Finder()
+S.add_Number(1)
+S.add_Number(2)
+result = S.find_Median()
+print(result)
+S.add_Number(3)
+result = S.find_Median()
+print(result)
+S.add_Number(4)
+S.add_Number(5)
+result = S.find_Median()
+print(result)
